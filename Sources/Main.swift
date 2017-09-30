c1dd9003311ce5bea00946ccc610e297a74263ba
Luis de la Rosa <luis@luisdelarosa.com>
2017-09-30 01:39:43 -0400

    Changed sensor for DHT11. Sent humidity to MQTT via shell.

--- Sources/Main.swift
+++ Sources/Main.swift
@@ -6,17 +6,39 @@ import dhtxx
 let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPi3)
 
 let pin27 = gpios[.P27]!
-let dht_sensor_type = dhtxx.SupportedSensor.dht22
+let dht_sensor_type = dhtxx.SupportedSensor.dht11
 
 let dht_sensor = DHT(pin: pin27, for: dht_sensor_type)
 
 var temperature = 0.0
 var humidity = 0.0
 
+// From https://stackoverflow.com/a/26973384/1174526
+@discardableResult
+func shell(_ args: String...) -> Int32 {
+    let task = Process()
+    task.launchPath = "/usr/bin/env"
+    task.arguments = args
+    task.launch()
+    task.waitUntilExit()
+    return task.terminationStatus
+}
+
 while (true) {
 	do {
+		// Read sensor
 		(temperature, humidity) = try dht_sensor.read(debug: false)
-		print("temperature: \(temperature) humidity: \(humidity)")
+
+		if (humidity < 100.0) {
+
+			// Temporary hack until we have a supported MQTT module
+			shell("mosquitto_pub", "-t", "test", "-m", "\(humidity)")
+
+			// Log on console
+			print("temperature: \(temperature) humidity: \(humidity)")
+		} else {
+			print("error reading humidity")
+		}
 	} catch {
 		print("error while reading temperature and humidity")
 	}
